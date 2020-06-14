b4aa3af09b0ceff17f76f2b43051f241e0e2c108
francomanca93 <francomanca93@gmail.com>
2020-06-14 08:17:30 -0300

    Practicando con el algoritmo de ordenamiento por mezcla

---
+++ poo-y-algoritmos/ordenamiento_mezcla.py
@@ -0,0 +1,57 @@
+# Algoritmo de ordenamiento por mezcla
+
+import random
+
+def ordenamiento_mezcla(lista):
+    if len(lista) > 1:
+        medio = len(lista) // 2
+        izquierda = lista[:medio]
+        derecha = lista[medio:]
+        print(izquierda, '*' * 5, derecha)
+        # Llamada recursiva en cada mitad para obtener las listas mas pequeñas posibles. Crecimiento log n
+        ordenamiento_mezcla(izquierda)
+        ordenamiento_mezcla(derecha)
+
+        # Iteradores para recorrer las dos sublistas
+        i = 0
+        j = 0
+        # Iterador para la lista principal
+        k = 0
+
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+            else:
+                lista[k] = derecha[j]
+                j += 1
+            
+            k += 1
+        
+        while i < len(izquierda):
+            lista[k] = izquierda[i]
+            i += 1
+            k += 1
+        
+        while j < len(derecha):
+            lista[k] = derecha[j]
+            j += 1
+            k += 1
+
+        print(f'izquierda {izquierda}, derecha {derecha}')
+        print(lista)
+        print('-' * 50)
+
+    return lista
+
+
+
+if __name__ == '__main__':
+    tamano_de_lista = int(input('De que tamaño sera la lista? '))
+    
+    lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
+    print(lista)
+    print('-' * 20)
+
+    lista_ordenada = ordenamiento_mezcla(lista)
+    print(lista_ordenada)
